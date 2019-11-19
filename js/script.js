b1952e9500323727590359eda699b3c0c99889e0
Brendan <brendan.garvey@queensu.ca>
2019-11-19 23:59:40 +0100

    Finished gameplay and scoreboard in process

--- js/script.js
+++ js/script.js
@@ -1,26 +1,52 @@
 var listOfNumbers = ["1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8","9","9","10","10","11","11","12","12","13","13","14","14","15","15"];
 var listOfClasses = ["a0","a1","a2","a3","a4","a5","a6","a7","a8","a9","b0","b1","b2","b3","b4","b5","b6","b7","b8","b9","c0","c1","c2","c3","c4","c5","c6","c7","c8","c9"];
 var secondGuess = false;
+var wait = false;
 
 $(function(){
 
   shuffle(listOfNumbers);
   start();
-
+  var num1 = "";
+  var num2 = "";
+  var class1 = "";
+  var class2 = "";
   $(".hide").click(function(){
-    $(this).removeClass("hide");
-    if (secondGuess === false){
-      secondGuess = true;
-      var classes = $(this).attr("class");
-      var classes_arr  = classes.split(" ");
-      var letter = classes_arr[1];
-      var num = listOfNumbers[listOfClasses.indexOf(letter)];
-      console.log(letter);
-      console.log(num);
-    }
-    else{
-      secondGuess = false;
-    }
+
+
+
+      if (secondGuess === false && wait === false && $(this).attr("class").includes("hide")){
+        $(this).removeClass("hide");
+        secondGuess = true;
+        var classes = $(this).attr("class");
+        var classes_arr  = classes.split(" ");
+        class1 = classes_arr[1];
+        num1 = listOfNumbers[listOfClasses.indexOf(class1)];
+        console.log(num1);
+      }
+      else if (secondGuess === true && wait === false && $(this).attr("class").includes("hide")){
+        $(this).removeClass("hide").delay(1000);
+        secondGuess = false;
+        var classes = $(this).attr("class");
+        var classes_arr  = classes.split(" ");
+        class2 = classes_arr[1];
+        num2 = listOfNumbers[listOfClasses.indexOf(class2)];
+        console.log(num2);
+        if(num1 === num2 && class1 !== class2){
+          console.log("Matched!");
+          wait = true;
+          sleepClear(class1, class2);
+
+        }
+        else{
+          console.log("no match")
+          wait = true;
+          sleepHide(class1, class2);
+        }
+      }
+
+
+
 
 
   });
@@ -29,6 +55,25 @@ $(function(){
 
 
 });
+
+
+
+
+function sleepClear(class1, class2) {
+  setTimeout(function(){
+    $("."+class1).html("");
+    $("."+class2).html("");
+    wait = false;
+  }, 1000);
+};
+
+function sleepHide(class1, class2) {
+  setTimeout(function(){
+    $("."+class1).addClass("hide");
+    $("."+class2).addClass("hide");
+    wait = false;
+  }, 1000);
+};
 
 
 
@@ -44,9 +89,9 @@ function start(){
 
 
 function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
 }
