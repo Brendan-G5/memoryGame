378646a3eb6491d6e13babc515915401d83c8066
Brendan <brendan.garvey@queensu.ca>
2019-11-19 19:38:54 +0100

    Basic Java for layout and clicking

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+  $(".card").click(function(){
+    console.log("Card Clicked");
+    $(this).css("background-color","rgba(255, 0, 0, 0)");
+  });
+});
+
+
+var listOfNumbers = ["0","0","1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8","9","9","10","10","11","11","12","12","13","14","14","15","15"];
+var listOfClasses = ["a0","a1","a2","a3","a4","a5","a6","a7","a8","a9","b0","b1","b2","b3","b4","b5","b6","b7","b8","b9","c0","c1","c2","c3","c4","c5","c6","c7","c8","c9"];
+
+for (var i = 0; i<listOfClasses;i++){
+  
+}
